cb7f396d0fa61638d40b5f511cd64bc0a6c705ef
Laurel German <laurel.german@terazo.com>
2021-10-13 16:39:09 -0400

    Add app called 'projects' with some views and templates

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def projects(request): #request is an HTTP request object
+    return render(request, 'projects/projects.html')
+
+def project(request, pk): #pk: dynamic value
+    return render(request, 'projects/single-project.html')
+    # if not using template: return HttpResponse('SINGLE PROJECT ' + str(pk))
