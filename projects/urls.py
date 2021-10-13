6e2ab5402e069f0385a437d96645874f79b2c0d4
Laurel German <laurel.german@terazo.com>
2021-10-13 17:18:18 -0400

    Add dynamic urls and work with curly-brace tags and variables

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('projects/',views.projects, name="projects"),
+    path('',views.projects, name="projects"), #empty string means this is the root domain
     path('project/<str:pk>/',views.project, name="project"), #pk = primary key; can also use slug or int
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def projects(request): #request is an HTTP request object
-    return render(request, 'projects/projects.html')
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community'
+    }
+]
 
-def project(request, pk): #pk: dynamic value
-    return render(request, 'projects/single-project.html')
+
+
+
+def projects(request): #request is an HTTP request object
+    page = 'projects'
+    number = 10
+    context = {'page': page, 'number': number, 'projects':projectsList}
+    return render(request, 'projects/projects.html', context)
+
+def project(request, pk): #pk: dynamic value (primary key)
+    projectObj = None
+    for i in projectsList: #iterate thru project list, if something matches the primary key, return that one as an object
+        if i['id'] == pk:
+            projectObj = i
+    return render(request, 'projects/single-project.html', {'project':projectObj})
     # if not using template: return HttpResponse('SINGLE PROJECT ' + str(pk))
