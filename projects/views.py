3fdfbc85957b5d331fc0bf17aec7821b1047427c
Laurel German <laurel.german@terazo.com>
2021-10-16 17:18:20 -0400

    Add database queries to replace dummy data

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
 
 projectsList = [
     {
@@ -23,15 +24,12 @@ projectsList = [
 
 
 def projects(request): #request is an HTTP request object
-    page = 'projects'
-    number = 10
-    context = {'page': page, 'number': number, 'projects':projectsList}
+    projects = Project.objects.all() #query to get all Project objects
+    context = {'projects':projects}
     return render(request, 'projects/projects.html', context)
 
 def project(request, pk): #pk: dynamic value (primary key)
-    projectObj = None
-    for i in projectsList: #iterate thru project list, if something matches the primary key, return that one as an object
-        if i['id'] == pk:
-            projectObj = i
+    projectObj = Project.objects.get(id=pk)
+    #tags = projectObj.tags.all()
     return render(request, 'projects/single-project.html', {'project':projectObj})
     # if not using template: return HttpResponse('SINGLE PROJECT ' + str(pk))
