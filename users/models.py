37f8ed3b7d52e6ba80c30e5917691b7d9cd1a2d4
Laurel German <laurel.german@terazo.com>
2021-11-11 12:25:18 -0500

    Start adding signals

--- users/models.py
+++ users/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User #built-in user model
 import uuid
 
-# Create your models here.
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver #this is for using decorators
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True) #on_delete maintains one-to-one relationship
@@ -22,4 +24,17 @@ class Profile(models.Model):
 
     
     def __str__(self):
-        return str(self.user.username) #wrapping it in str ensures it's a string in case username doesn't exist or is an integer or something
+        return str(self.username) #wrapping it in str ensures it's a string in case username doesn't exist or is an integer or something
+
+
+#@receiver(post_save, sender=Profile)
+def profileUpdated(sender, instance, created, **kwargs):
+    print('Profile Saved!')
+    print('Instance:', instance)
+    print('CREATED:', created)
+
+def deleteUser(sender, instance, **kwargs):
+    print('Deleting user...')
+
+post_save.connect(profileUpdated, sender=Profile)
+post_delete.connect(deleteUser, sender=Profile)
